dac997e5ef15205d2ef63609f1e647e8da13cd1c
Deepak0Prajapati <deepakprajapatibluethink@gmail.com>
2025-08-24 14:00:05 +0530

    Removed node_modules from git tracking

--- back-end/server.js
+++ back-end/server.js
@@ -2,16 +2,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/product.js";
+import salesRoutes from "./routes/sales.js"
+
 
 const app = express();
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/sales", salesRoutes);
+
+
 
 // Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/warehouse", {
